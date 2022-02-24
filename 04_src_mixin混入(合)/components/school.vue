efab8329638853913d71aae654d7347f55288909
kongdehui <995533366@qq.com>
2022-02-25 00:03:15 +0800

    20220224

---
+++ 04_src_mixin混入(合)/components/school.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2 @click="showName">学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+  </div>
+</template>
+
+<script>
+  // 引入hunhe
+  // import {hunhe,hunhe2} from "../mixin"
+    export default {
+      name:"School",
+      data() {
+        return {
+          name:"上端",
+          address:"广州"
+        }
+      },
+      // mixins:[hunhe,hunhe2]
+    }
+</script>
---
+++ 04_src_mixin混入(合)/components/stundent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2 @click="showName">学生姓名：{{name}}</h2>
+    <h2>学生性别：{{sex}}</h2>
+  </div>
+</template>
+
+<script>
+  // 引入hunhe
+  // import {hunhe,hunhe2} from "../mixin"
+    export default {
+      name:"Student",
+      data() {
+        return {
+          name:"小花",
+          sex:"女"
+        }
+      },
+      // mixins:[hunhe,hunhe2]
+    }
+  </script>
