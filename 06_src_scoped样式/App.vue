c34a8dd763dfcf0b1e81ac5b1d3edfa240e18fbd
kongdehui <995533366@qq.com>
2022-02-25 22:20:51 +0800

    20220225

---
+++ 06_src_scoped样式/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="title">您好</h1>
+    <Student/>
+    <hr>
+    <School/>
+  </div>
+</template>
+
+<script>
+  // 引入组件
+  import Student from "./components/stundent.vue"
+  import School from "./components/school.vue"
+  export default {
+    name:"App",
+    components:{
+      Student,
+      School
+    },
+  }
+</script>
+
+<style scoped>
+  .title {
+    color:red
+  }
+</style>
