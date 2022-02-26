5b53af597954ceec3f76da7ab039a60efafd11b0
kongdehui <995533366@qq.com>
2022-02-26 17:00:25 +0800

    2022022617

---
+++ src/components/stundent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="student">
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+    <button @click="sendStudentName">把学生名给App</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Student",
+    data() {
+      return {
+        name: "小花",
+        sex: "女",
+      };
+    },
+    methods: {
+      sendStudentName(){
+        // 触发Student组件实例身上的getStuNa事件
+        this.$emit("getStuNa",this.name,666,888,999)
+      }
+    },
+  };
+</script>
+
+<style lang="less">
+  .student {
+    background-color: hotpink;
+    padding: 5px;
+  }
+</style>
